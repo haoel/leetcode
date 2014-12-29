5d6429d79f2fde0fc6e0edcb180bb4575d1d771f
Hao Chen <haoel@hotmail.com>
2014-12-29 20:50:57 +0800

    add a case to check if the return value is overflowed, and slight optimization

--- src/divideTwoInt/divideTwoInt.cpp
+++ src/divideTwoInt/divideTwoInt.cpp
@@ -6,7 +6,8 @@
 * 
 * Divide two integers without using multiplication, division and mod operator.
 * 
-*               
+* If it is overflow, return MAX_INT. 
+*
 **********************************************************************************/
 
 #include <stdio.h>
@@ -14,25 +15,17 @@
 #include <iostream>
 using namespace std;
 
+#define INT_MAX     2147483647
+#define INT_MIN     (-INT_MAX - 1)
 
 int divide(int dividend, int divisor) {
-  
-    int sign = 1;
-    long long dvd = dividend;
-    long long dvs = divisor;
-    if (dvd<0) {
-        dvd = -dvd;
-        sign = -sign;
-    } 
-    if (dvs<0) {
-        dvs = -dvs;
-        sign = -sign;
-    }
 
-    long long bit_num[32];
-    memset( bit_num, 0, sizeof(bit_num) );
+    int sign = (float)dividend / divisor > 0 ? 1 : -1;
+    unsigned int dvd = dividend > 0 ? dividend : -dividend;
+    unsigned int dvs = divisor > 0 ? divisor : -divisor;
 
-    int i=0;
+    unsigned int bit_num[32];
+    unsigned int i=0;
     long long d = dvs;
     bit_num[i] = d;
     while( d <= dvd ){
@@ -50,18 +43,24 @@ int divide(int dividend, int divisor) {
         }
     }
 
-    return result * sign;
+    //becasue need to return `int`, so we need to check it is overflowed or not.
+    if ( result > INT_MAX && sign > 0 ) {
+        return INT_MAX;
+    }
+    return (int)result * sign;
 }
 
 
 int main()
 {
+    cout << "0/2=" << divide(0, 2) << endl;
     cout << "10/2=" << divide(10, 2) << endl;
     cout << "10/3=" << divide(10, 3) << endl;
     cout << "10/5=" << divide(10, 5) << endl;
     cout << "10/7=" << divide(10, 7) << endl;
     cout << "10/10=" << divide(10, 10) << endl;
     cout << "10/11=" << divide(10, 11) << endl;
+    cout << "-1/1=" << divide(1, -1) << endl;
     cout << "1/-1=" << divide(1, -1) << endl;
     cout << "-1/-1=" << divide(-1, -1) << endl;
     cout << "2147483647/1=" << divide(2147483647, 1) << endl;
@@ -71,4 +70,5 @@ int main()
     cout << "2147483647/2=" << divide(2147483647, 2) << endl;
     cout << "2147483647/10=" << divide(2147483647, 10) << endl;
     cout << "-2147483648/1=" << divide(-2147483648, 1) << endl;
+    cout << "-2147483648/-1=" << divide(-2147483648, -1) << endl;
 }
