175976a86909d068485a2b1ab195388e25148eae
Hao Chen <haoel@hotmail.com>
2014-12-30 18:01:28 +0800

    another solution for "Factorial Trailing Zeroes"

--- src/factorialTrailingZeroes/factorialTrailingZeroes.cpp
+++ src/factorialTrailingZeroes/factorialTrailingZeroes.cpp
@@ -70,4 +70,17 @@ class Solution {
             }
             return result;
         }
+
+        // Alternative implementation which naturally avoid integer overflow issue.
+        int trailingZeroes(int n) {
+            int sum=0;
+            int tmp=0;
+            while(n/5>0)
+            {
+                tmp=n/5;
+                sum+=tmp;
+                n=tmp;
+            }
+            return sum;
+        }
 };
