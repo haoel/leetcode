3eea06551a830042a2421341930e951580924f43
Hao Chen <haoel@hotmail.com>
2015-06-09 23:42:35 +0800

    adjust the comment length

--- algorithms/minimumSizeSubarraySum/MinimumSizeSubarraySum.cpp
+++ algorithms/minimumSizeSubarraySum/MinimumSizeSubarraySum.cpp
@@ -4,7 +4,8 @@
 
 /********************************************************************************** 
  * 
- * Given an array of n positive integers and a positive integer s, find the minimal length of a subarray of which the sum ≥ s. If there isn't one, return 0 instead.
+ * Given an array of n positive integers and a positive integer s, find the minimal length of a subarray 
+ * of which the sum ≥ s. If there isn't one, return 0 instead.
  * 
  * For example, given the array [2,3,1,2,4,3] and s = 7,
  * the subarray [4,3] has the minimal length under the problem constraint.
