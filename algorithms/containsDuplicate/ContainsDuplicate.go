e6adfea71784f7e27ccb9982a86c86d7eaa14af2
yanglong <yanglong@rijin.com>
2015-06-30 16:30:54 +0800

    finish ContainsDuplicate

--- algorithms/containsDuplicate/ContainsDuplicate.go
+++ algorithms/containsDuplicate/ContainsDuplicate.go
@@ -1,5 +1,16 @@
 package containsDuplicate
 
+func containsDuplicate(nums []int) bool {
+	m := map[int]int{}
+	for _, v := range nums {
+		if _, ok := m[v]; ok {
+			return true
+		}
+		m[v] += 1
+	}
+	return false
+}
+
 func containsDuplicate2(nums []int, step int) bool {
 	m := map[int]int{}
 	for k, v := range nums {
--- algorithms/containsDuplicate/ContainsDuplicate_test.go
+++ algorithms/containsDuplicate/ContainsDuplicate_test.go
@@ -4,6 +4,18 @@ import (
 	"testing"
 )
 
+func TestContainsDuplicate(t *testing.T) {
+	nums := []int{1, 2, 3, 4, 5}
+	if containsDuplicate(nums) {
+		t.Error("fail")
+	}
+
+	nums = []int{1, 2, 3, 4, 5, 1}
+	if !containsDuplicate(nums) {
+		t.Error("fail")
+	}
+}
+
 func TestContainsDuplicate2(t *testing.T) {
 	nums := []int{1, 2, 3, 4, 5, 1}
 	if containsDuplicate2(nums, 4) {
