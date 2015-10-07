8d568d537a3bb7ac1e2f4cfc5e3e8b3dad3b3797
Lei Cao <lei.cao@zalora.com>
2015-10-07 18:50:58 +0800

    285 inorder successor in BST

---
+++ algorithms-java/src/inorderSuccessorInBST/inorderSuccessorInBSTTest.java
@@ -0,0 +1,53 @@
+package inorderSuccessorInBST;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by leicao on 7/10/15.
+ */
+public class inorderSuccessorInBSTTest {
+
+    @Test
+    public void testInorderSuccessor() throws Exception {
+        ArrayList<TreeNode> inputes = new ArrayList<TreeNode>();
+        ArrayList<TreeNode> targets = new ArrayList<TreeNode>();
+
+        TreeNode n0 = new TreeNode(2);
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(3);
+        n0.left = n1;
+        n0.right = n2;
+        inputes.add(n0);
+        targets.add(n2);
+
+        TreeNode nn0 = new TreeNode(2);
+        TreeNode nn1 = new TreeNode(1);
+        nn0.left = nn1;
+        inputes.add(nn0);
+        targets.add(nn1);
+
+        TreeNode t0 = new TreeNode(1);
+        TreeNode t1 = new TreeNode(2);
+        t0.right = t1;
+        inputes.add(t0);
+        targets.add(t0);
+
+
+        ArrayList<TreeNode> results = new ArrayList<TreeNode>();
+        results.add(null);
+        results.add(nn0);
+        results.add(t1);
+
+        for (int i = 0; i < results.size(); i++) {
+            inorderSuccessorInBST finder = new inorderSuccessorInBST();
+            TreeNode node = finder.inorderSuccessor(inputes.get(i), targets.get(i));
+            System.out.println(node);
+            boolean result = node == results.get(i)  || node.val == results.get(i).val;
+            assertTrue(result);
+        }
+    }
+}
