60ccf4804e0272fa871b35e0da582fb2f9c1caef
Hao Chen <haoel@hotmail.com>
2015-10-19 16:12:50 +0800

    Support the MacOS (To be compatible with MacOS, some of code really to 'sed' are really mass)

--- scripts/comments.sh
+++ scripts/comments.sh
@@ -16,11 +16,23 @@ function usage()
     echo -e "Example:"
     echo -e ""
     echo -e "   1) Create a file named largestNumber.cpp, and add Copyright & Problem description"
-    echo -e "   ${0} https://oj.leetcode.com/problems/largest-number/"
+    echo -e "   ${0} https://leetcode.com/problems/largest-number/"
     echo -e ""
     echo -e "   2) Add Copyright & Problem description into existed file"
-    echo -e "   ${0} https://oj.leetcode.com/problems/largest-number/ largestNumber.cpp"
+    echo -e "   ${0} https://leetcode.com/problems/largest-number/ largestNumber.cpp"
     echo -e ""
+}
+
+function detect_os()
+{
+    platform='unknown'
+    ostype=`uname`
+    if [[ "$ostype" == 'Linux' ]]; then
+       platform='linux'
+    elif [[ "$ostype" == 'Darwin' ]]; then
+       platform='macos'
+    fi
+    echo ${platform}
 }
 
 function install_xidel()
@@ -30,25 +42,50 @@ function install_xidel()
         mkdir xidel
     fi
     cd xidel
-    linux=`uname -m`
-    xidel_tar=xidel-0.8.4.linux64.tar.gz
-    case $linux in
-        x86_64 )    xidel_tar=xidel-0.8.4.linux64.tar.gz
-                    ;;
-          i686 )    xidel_tar=xidel-0.8.4.linux32.tar.gz
-                    ;;
-             * )    echo "Cannot install xidel, please install it manually!"
-                    exit 1;
-    esac
-    if [ ! -f ${xidel_tar} ]; then
-        echo "Downloading xidel....."
-        curl -L http://softlayer-sng.dl.sourceforge.net/project/videlibri/Xidel/Xidel%200.8.4/${xidel_tar} -o ${xidel_tar}
+
+    platform=`detect_os`
+
+    if [[ "$platform" == "unknown" ]]; then
+        echo "Unknown platform, please install 'xidel' manually!"
+        exit 1;
     fi
-    tar -zxvf ${xidel_tar}
-    ./install.sh
+
+    #install the xidel on Linux platform
+    if [[ "$platform" == "linux" ]]; then
+        hardware=`uname -m`
+        xidel_tar=xidel-0.8.4.linux64.tar.gz
+        case $hardware in
+            x86_64 )    xidel_tar=xidel-0.8.4.linux64.tar.gz
+                        ;;
+              i686 )    xidel_tar=xidel-0.8.4.linux32.tar.gz
+                        ;;
+                 * )    echo "Cannot install xidel, please install it manually!"
+                        exit 1;
+        esac
+        if [ ! -f ${xidel_tar} ]; then
+            echo "Downloading xidel......"
+            curl -L http://softlayer-sng.dl.sourceforge.net/project/videlibri/Xidel/Xidel%200.8.4/${xidel_tar} -o ${xidel_tar}
+        fi
+        tar -zxvf ${xidel_tar}
+        ./install.sh
+    fi
+
+    #install the xidel on MacOS platform
+    #refer to: https://www.evernote.com/shard/s69/sh/ff1e78f3-a369-4855-b18f-6184ce789c45/f3511927d0fb356ce883835f2eb712e0
+    if [[ "$platform" == "macos" ]]; then
+        echo "Downloading xidel......" 
+        xidel_zip=xidel.zip
+        if [ ! -f ${xidel_zip} ]; then
+            curl -L https://www.evernote.com/shard/s69/sh/ff1e78f3-a369-4855-b18f-6184ce789c45/f3511927d0fb356ce883835f2eb712e0/res/9f156868-01b4-4838-9c2f-935d7a236e05/${xidel_zip} -o ${xidel_zip}
+        fi 
+        unzip ${xidel_zip}
+        mv xidel /usr/local/bin/
+    fi
+
     cd ..
     echo "Install xidel successfullly !"
 }
+
 
 if [ $# -lt 1 ] || [[ "${1}" != ${LEETCODE_NEW_URL}* ]] && [[ "${1}" != ${LEETCODE_OLD_URL}* ]]; then
     usage
@@ -68,10 +105,15 @@ fi
 
 leetcode_url=$1
 current_time=`date +%Y-%m-%d`
+platform=`detect_os`
 
 if [ $# -gt 1 ] && [ -f $2 ]; then
     source_file=$2
-    current_time=`stat -c %x ${source_file} | awk '{print \$1}'`
+    if [[ "$platform" == "linux" ]]; then
+        current_time=`stat -c %x ${source_file} | awk '{print \$1}'`
+    elif [[ "$platform" == "macos" ]]; then
+        current_time=`stat -f %a ${source_file} | xargs -I time date -r time +%Y-%m-%d`
+    fi
 else
     source_file=${1#${LEETCODE_URL}}
     source_file=${source_file::${#source_file}-1}
@@ -82,7 +124,11 @@ else
         echo -e "\n" > ${source_file}
         current_time=`date +%Y-%m-%d`
     else
-        current_time=`stat -c %x ${source_file} | awk '{print \$1}'`
+        if [[ "$platform" == "linux" ]]; then
+            current_time=`stat -c %x ${source_file} | awk '{print \$1}'`
+        elif [[ "$platform" == "macos" ]]; then
+            current_time=`stat -f %a ${source_file} | xargs -I time date -r time +%Y-%m-%d`
+        fi
     fi
 fi
 
@@ -94,13 +140,16 @@ fi
 
 #adding the Copyright Comments
 if  ! grep -Fq  "${COMMENT_TAG} Author :" $source_file ; then
-    sed -i '1i\'"${COMMENT_TAG} Source : ${leetcode_url}" $source_file
-    sed -i '2i\'"${COMMENT_TAG} Author : ${AUTHOR}" $source_file
-    sed -i '3i\'"${COMMENT_TAG} Date   : ${current_time}\n" $source_file
+    sed -i.bak '1i\'$'\n'"${COMMENT_TAG} Source : ${leetcode_url}"$'\n' $source_file
+    sed -i.bak '2i\'$'\n'"${COMMENT_TAG} Author : ${AUTHOR}"$'\n' $source_file
+    sed -i.bak '3i\'$'\n'"${COMMENT_TAG} Date   : ${current_time}"$'\n' $source_file
+    sed -i.bak '4i\'$'\n'""$'\n' $source_file
+    rm ${source_file}.bak
 fi
 
 #grab the problem description and add the comments
-xidel=`type -P xidel || /bin/true`
+TRUE_CMD=`which true`
+xidel=`type -P xidel || ${TRUE_CMD}`
 if [ -z "${xidel}" ]; then
     echo "xidel not found !"
     install_xidel
@@ -111,14 +160,17 @@ fi
 # 2) the last two `sed` commands are used to add the comments tags
 case $FILE_EXT in
     .cpp )      xidel ${leetcode_url} -q -e "css('div.question-content')"  | \
-                    grep -v '                ' |sed '/^$/N;/^\n$/D'  | fold -w 85 -s |\
-                    sed 's/^/ * /' | sed "1i/*$(printf '%.0s*' {0..80}) \n * " | \
-                    sed "\$a \ $(printf '%.0s*' {0..80})*/\n" > /tmp/tmp.txt
+                    grep -v '                ' | sed '/^$/N;/^\n$/D'  | fold -w 85 -s |\
+                    sed 's/^/ * /' | sed '1i\'$'\n'"/*$(printf '%.0s*' {0..85}) "$'\n' |\
+                    sed '2i\'$'\n''!@#$%'$'\n' | sed 's/!@#$%/ */' | \
+                    sed '$a\'$'\n'"*$(printf '%.0s*' {0..85})*/"$'\n'| \
+                    sed 's/^*/ /' > /tmp/tmp.txt
                 ;;
     .sh )      xidel ${leetcode_url} -q -e "css('div.question-content')"  | \
                     grep -v '                ' |sed '/^$/N;/^\n$/D'  | fold -w 85 -s| \
-                    sed 's/^/# /' | sed "1i#$(printf '%.0s#' {0..80}) \n# " | \
-                    sed "\$a \#$(printf '%.0s#' {0..80})\n" > /tmp/tmp.txt
+                    sed 's/^/# /' | sed '1i\'$'\n'"#$(printf '%.0s#' {0..85}) "$'\n' | \
+                    sed '2i\'$'\n''#'$'\n' | sed '$a\'$'\n'"#$(printf '%.0s#' {0..85})"$'\n'\
+                    > /tmp/tmp.txt
                 ;;
       * )       echo "Bad file extension!"
                 exit 1;
@@ -126,7 +178,8 @@ case $FILE_EXT in
 esac
 
 #insert the problem description into the source file, and remove it
-sed -i '4 r /tmp/tmp.txt' ${source_file}
+sed.bak -i "" '4 r /tmp/tmp.txt' ${source_file}
+rm -f ${source_file}.bak
 rm -f /tmp/tmp.txt
 
 echo "${source_file} updated !"
