8c3448aecb8939f7ee51884d09bd1c1f14343a49
Hao Chen <haoel@hotmail.com>
2015-10-23 08:41:27 +0800

    correct the algorithm to meet the problem's requirement - the array is ready only

--- algorithms/cpp/findTheDuplicateNumber/findTheDuplicateNumber.cpp
+++ algorithms/cpp/findTheDuplicateNumber/findTheDuplicateNumber.cpp
@@ -1,5 +1,5 @@
 // Source : https://leetcode.com/problems/find-the-duplicate-number/
-// Author : Calinescu Valentin
+// Author : Hao Chen, Calinescu Valentin
 // Date   : 2015-10-19
 
 /*************************************************************************************** 
@@ -9,11 +9,12 @@
  * Assume that there is only one duplicate number, find the duplicate one.
  *
  * Note:
- * You must not modify the array (assume the array is read only).
- * You must use only constant, O(1) extra space.
- * Your runtime complexity should be less than O(n2).
- * There is only one duplicate number in the array, but it could be repeated more than
- * once.
+ * > You must not modify the array (assume the array is read only).
+ * > You must use only constant, O(1) extra space.
+ * > Your runtime complexity should be less than O(n2).
+ * > There is only one duplicate number in the array, but it could be repeated more than
+ *   once.
+ *
  * Credits:
  * Special thanks to @jianchao.li.fighter for adding this problem and creating all test
  * cases.
@@ -21,24 +22,31 @@
  ***************************************************************************************/
 
 
-
-/* 
- * Solutions
- * =========
- *
- * A simple solution would be to sort the array and then look for equal consecutive elements.
- * 
- * Time Complexity: O(N log N)
- * Space Complexity: O(1)
- * 
- */
-#include <algorithm>
 class Solution {
 public:
+    //
+    // This problem can be transfromed to "Linked List Cycle" problem.
+    // There are two pointers, one goes one step, another goes two steps.
+    //
+    // Refer to: https://en.wikipedia.org/wiki/Cycle_detection
+    //
     int findDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        for(vector<int>::iterator it = nums.begin(); it != nums.end(); ++it)
-            if(*it == *(it + 1))
-                return *it;
+        int n = nums.size();
+        int one = n;
+        int two = n;
+
+       do{
+            one = nums[one-1];
+            two = nums[nums[two-1]-1];
+        } while(one != two); 
+        
+        //find the start point of the cycle
+        one = n;
+        while(one != two){
+            one = nums[one-1];
+            two = nums[two-1];
+        }
+        
+        return one;
     }
 };
