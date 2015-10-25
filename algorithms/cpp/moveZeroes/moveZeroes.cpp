1ba3b95ba7ee558f9f548ba3dbd9cb368a63d94c
Hao Chen <haoel@hotmail.com>
2015-10-25 21:29:38 +0800

    Another implemtation which is easy to understand

--- algorithms/cpp/moveZeroes/moveZeroes.cpp
+++ algorithms/cpp/moveZeroes/moveZeroes.cpp
@@ -1,5 +1,5 @@
 // Source : https://leetcode.com/problems/move-zeroes/
-// Author : Calinescu Valentin
+// Author : Calinescu Valentin, Hao Chen
 // Date   : 2015-10-21
 
 /*************************************************************************************** 
@@ -21,22 +21,22 @@
 
 
 
-/* 
- * Solutions
- * =========
- *
- * One solution would be to store the position of the next non-zero element of the array.
- * Every position of the array, starting with position 0, must store this next non-zero
- * element until we can no more do that, case in which we need to add the remaining zeros
- * that we skipped.
- * 
- * 
- * Time Complexity: O(N)
- * Space Complexity: O(1)
- * 
- */
 class Solution {
 public:
+    /* 
+     * Solution (Calinescu Valentin)
+     * ==============================
+     *
+     * One solution would be to store the position of the next non-zero element of the array.
+     * Every position of the array, starting with position 0, must store this next non-zero
+     * element until we can no more do that, case in which we need to add the remaining zeros
+     * that we skipped.
+     * 
+     * 
+     * Time Complexity: O(N)
+     * Space Complexity: O(1)
+     * 
+     */
     void moveZeroes(vector<int>& nums) {
         int i = 0, poz = 0;
         for(i = 0; i < nums.size() && poz < nums.size(); i++)
@@ -52,4 +52,35 @@ public:
         for(; i < nums.size(); i++)
             nums[i] = 0;
     }
+
+
+
+    /*
+     * Another implemtation which is easy to understand (Hao Chen)
+     * ===========================================================
+     *
+     * We have two pointers to travel the array, assume they named `p1` and `p2`.
+     * 
+     *   1) `p1` points the tail of current arrays without any ZEROs.
+     *   2) `p2` points the head of the reset array which skips the ZEROs.
+     * 
+     * Then we can just simply move the item from `p2` to `p1`.
+     *
+     */
+    void moveZeroes(vector<int>& nums) {
+        int p1=0, p2=0;
+
+        // Find the first ZERO, where is the tail of the array.
+        // (Notes: we can simply remove this!)
+        for (; nums[p1]!=0 && p1<nums.size(); p1++);
+        
+        // copy the item from p2 to p1, and skip the ZERO
+        for (p2=p1; p2<nums.size(); p2++) {
+            if ( nums[p2] == 0 ) continue;
+            nums[p1++] = nums[p2]; 
+        }    
+        
+        while ( p1<nums.size() ) nums[p1++] = 0;
+    }
+
 };
