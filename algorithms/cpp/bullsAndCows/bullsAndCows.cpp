4a7fb6fe5f34c4a754cee2a993be4713b83c4992
Hao Chen <haoel@hotmail.com>
2015-11-10 18:34:15 +0800

    another implementation

--- algorithms/cpp/bullsAndCows/bullsAndCows.cpp
+++ algorithms/cpp/bullsAndCows/bullsAndCows.cpp
@@ -1,5 +1,5 @@
 // Source : https://leetcode.com/problems/bulls-and-cows/
-// Author : Calinescu Valentin
+// Author : Calinescu Valentin, Hao Chen
 // Date   : 2015-11-04
 
 /*************************************************************************************** 
@@ -36,9 +36,14 @@
  
 class Solution {
 public:
-    int appears_in_secret[10], appears_in_guess[10], bulls[10];
-    int total_bulls, total_cows;
     string getHint(string secret, string guess) {
+        return getHint02(secret, guess);
+        return getHint01(secret, guess);
+    }
+
+    string getHint01(string secret, string guess) {
+        int appears_in_secret[10], appears_in_guess[10], bulls[10];
+        int total_bulls, total_cows;
         for(int i = 0; i < secret.size(); i++)
             appears_in_secret[secret[i] - '0']++;
         for(int i = 0; i < guess.size(); i++)
@@ -53,5 +58,33 @@ public:
             total_cows += min(appears_in_secret[i], appears_in_guess[i]) - bulls[i];
         }
         return to_string(total_bulls) + "A" + to_string(total_cows) + "B";
+    }
+
+    // Another implemntation - to save more space
+    string getHint02(string secret, string guess) {
+        
+        const int digital_num = 10;
+        int secret_stat[digital_num]={0};
+        
+        int bull = 0;
+        for(int i=0; i<secret.size(); i++) {
+            //both number & location are matched, count bull
+            if ( secret[i] == guess[i] ) {
+                bull++;
+                continue;
+            }
+            //otherwise, count the unmatched digits.
+            secret_stat[secret[i]-'0']++;
+        }
+        
+        int cow = 0;
+        for(int i=0; i<guess.size(); i++) {
+            //deal with duplication - decrease the digits count if cow is found.
+            if ( secret[i] != guess[i] && secret_stat[guess[i]-'0']-- > 0 ) {
+                cow++;
+            }
+        }
+        
+        return to_string(bull) + "A" + to_string(cow) + "B";
     }
 };
