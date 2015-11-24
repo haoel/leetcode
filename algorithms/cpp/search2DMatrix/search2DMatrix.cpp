9dd87d49e4a9270cb076865c8ca9618833c78239
Hao Chen <haoel@hotmail.com>
2015-11-25 00:00:03 +0800

    simpler solution

--- algorithms/cpp/search2DMatrix/search2DMatrix.cpp
+++ algorithms/cpp/search2DMatrix/search2DMatrix.cpp
@@ -26,7 +26,35 @@
 
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int> > &matrix, int target) {
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        return searchMatrix01(matrix, target);
+        return searchMatrix02(matrix, target);
+    }
+
+    //Just simply convert the 2D matrix to 1D array.
+    bool searchMatrix01(vector<vector<int>>& matrix, int target) {
+        int row = matrix.size();
+        int col = row>0 ? matrix[0].size() : 0;
+        
+        int len = row * col;
+        int low = 0, high = len -1;
+        while (low <= high) {
+            
+            int mid = low + (high - low) / 2;
+            int r = mid / col;
+            int c = mid % col;
+ 
+            int n = matrix[r][c];
+            if (n == target) return true;
+            if (n < target) low = mid+1;
+            else high = mid -1;
+        }
+        return false;
+    }
+
+
+
+    bool searchMatrix02(vector<vector<int> > &matrix, int target) {
         
         int idx = vertical_binary_search(matrix, target);
         if (idx<0){
