1a64b361e9716f4316e133cb7074819e797d2a86
Hao Chen <haoel@hotmail.com>
2015-11-24 22:56:22 +0800

    refactory the code

--- algorithms/cpp/removeElement/removeElement.cpp
+++ algorithms/cpp/removeElement/removeElement.cpp
@@ -13,6 +13,16 @@
 
 class Solution {
 public:
+    int removeElement(vector<int>& nums, int val) {
+        int pos = 0;
+        for (int i=0; i<nums.size(); i++){
+            if (nums[i] != val){
+                nums[pos++] = nums[i]; 
+            }
+        }
+        return pos;
+    }
+
     int removeElement(int A[], int n, int elem) {
         int tail = n-1;
         int i = 0;
