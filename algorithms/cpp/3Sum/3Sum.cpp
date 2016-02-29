dfb546a016f2594662877eef199f47602de2a5c2
Hao Chen <haoel@hotmail.com>
2016-02-29 17:43:33 +0800

    fix #96

--- algorithms/cpp/3Sum/3Sum.cpp
+++ algorithms/cpp/3Sum/3Sum.cpp
@@ -71,7 +71,7 @@ vector<vector<int> > threeSum(vector<int> &num) {
                 result.push_back(v);
                 // Continue search for all triplet combinations summing to zero.
                 //skip the duplication
-                while(low<n && num[low]==num[low+1]) low++; 
+                while(low<n-1 && num[low]==num[low+1]) low++; 
                 while(high>0 && num[high]==num[high-1]) high--; 
                 low++;
                 high--;
