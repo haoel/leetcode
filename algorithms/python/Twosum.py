b559b521f0d90b70a55634dc4786dab4226ba246
wenweizww@wenweideMBP <zhou_wenwei@wuxiapptec.com>
2017-10-29 13:33:25 +0800

    machineleanring

--- algorithms/python/Twosum.py
+++ algorithms/python/Twosum.py
@@ -16,6 +16,8 @@ import time
 
 
 class Solution(object):
+
+
     def twoSum(self, nums, target):
         """
         :type nums: List[int]
@@ -45,9 +47,45 @@ class Solution(object):
                 listPrimeData.append(i)
 
         return listPrimeData
+
     #expore more
     def primeDataOther(self,n):
         return filter(lambda x: not [x%i for i in range(2, int(math.sqrt(x))+1) if x%i ==0], range(2,n+1))
+
+    #
+    # There are two sorted arrays nums1 and nums2 of size m and n respectively.
+    #
+    # Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+    #
+    # Example 1:
+    # nums1 = [1, 3]
+    # nums2 = [2]
+    #
+    # The median is 2.0
+    # Example 2:
+    # nums1 = [1, 2]
+    # nums2 = [3, 4]
+    #
+    # The median is (2 + 3)/2 = 2.5
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        x = len(nums1)
+        y = len(nums2)
+        z = x+y
+
+        print z
+
+
+        for i in range(len(nums1)):
+
+
+
+
+
 
 
 
---
+++ algorithms/python/machineLearningInAction/tree.py
@@ -0,0 +1,16 @@
+from math import log
+
+def calcShannonEnt(dataSet):
+    numEntries = len(dataSet)
+    labelCounts = {}
+    for featVec in dataSet:
+        currentLabel = featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1
+    shannonEnt = 0.0
+    for key in labelCounts:
+        prob = float(labelCounts[key])/numEntries
+        shannonEnt -= prob* log(prob,2)
+    return shannonEnt
+
