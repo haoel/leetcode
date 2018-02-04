1197b9bc1b2a0d626249f3dd0875e5e4355580ac
Hao Chen <haoel@hotmail.com>
2018-02-04 12:14:05 +0800

    remove `DockerCircles` and add problem description for sentence sereen fitting

--- algorithms/cpp/sentenceScreenFitting/sentenceScreenFitting.cpp
+++ algorithms/cpp/sentenceScreenFitting/sentenceScreenFitting.cpp
@@ -6,7 +6,76 @@
 //  Copyright (c) 2016 Wang Yi. All rights reserved.
 //
 
-#include "OJ#418.h"
+
+/**********************************************************************************
+ *  
+ *  Given a rows x cols screen and a sentence represented by a list of words, find 
+ *  how many times the given sentence can be fitted on the screen.
+ *  
+ *  Note:
+ *  
+ *      A word cannot be split into two lines.
+ *      The order of words in the sentence must remain unchanged.
+ *      Two consecutive words in a line must be separated by a single space.
+ *      Total words in the sentence won't exceed 100.
+ *      Length of each word won't exceed 10.
+ *      1 ≤ rows, cols ≤ 20,000.
+ *  
+ *   
+ *  
+ *  Example 1:
+ *  
+ *  Input:
+ *  rows = 2, cols = 8, sentence = ["hello", "world"]
+ *  
+ *  Output: 
+ *  1
+ *  
+ *  Explanation:
+ *  hello---
+ *  world---
+ *  
+ *  The character '-' signifies an empty space on the screen.
+ *  
+ *   
+ *  
+ *  Example 2:
+ *  
+ *  Input:
+ *  rows = 3, cols = 6, sentence = ["a", "bcd", "e"]
+ *  
+ *  Output: 
+ *  2
+ *  
+ *  Explanation:
+ *  a-bcd- 
+ *  e-a---
+ *  bcd-e-
+ *  
+ *  The character '-' signifies an empty space on the screen.
+ *  
+ *   
+ *  
+ *  Example 3:
+ *  
+ *  Input:
+ *  rows = 4, cols = 5, sentence = ["I", "had", "apple", "pie"]
+ *  
+ *  Output: 
+ *  1
+ *  
+ *  Explanation:
+ *  I-had
+ *  apple
+ *  pie-I
+ *  had--
+ *  
+ *  The character '-' signifies an empty space on the screen.
+ *  
+ *  *********************************************************************************/
+
+
+#include "sentenceScreenFitting.h"
 #include <string.h>
 #include <iomanip>
 #include <iostream>
