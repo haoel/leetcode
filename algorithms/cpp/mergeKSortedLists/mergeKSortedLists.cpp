95e8b084042e328edabed90fd5393e193f22688b
Yi Wang <yi.wang.2005@gmail.com>
2018-03-05 22:13:48 -0800

    Rewrite mergeKSortedLists.cpp

--- algorithms/cpp/mergeKSortedLists/mergeKSortedLists.cpp
+++ algorithms/cpp/mergeKSortedLists/mergeKSortedLists.cpp
@@ -1,187 +1,53 @@
-// Source : https://oj.leetcode.com/problems/merge-k-sorted-lists/
-// Author : Hao Chen
-// Date   : 2014-07-06
-
-/********************************************************************************** 
-* 
-* Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
-* 
-*               
-**********************************************************************************/
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <algorithm>
+#include <cstdarg>
+#include <forward_list>
 #include <iostream>
+#include <utility>
 #include <vector>
-using namespace std;
 
+std::forward_list<int>* MergeKSortedLists(int k...) {
+  va_list args;
+  va_start(args, k);
 
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
-
-ListNode *mergeTwoLists(ListNode* head1, ListNode* head2);
-ListNode *mergeTwoLists01(ListNode* head1, ListNode* head2);
-ListNode *mergeTwoLists02(ListNode* head1, ListNode* head2);
-
-ListNode *mergeKLists(vector<ListNode *> &lists) {
-    ListNode *p, *p1, *p2;
-    while(lists.size()>1){
-        p1 = lists.back();    
-        lists.pop_back();
-        p2 = lists.back();    
-        lists.pop_back();
-        p = mergeTwoLists(p1, p2);
-        lists.insert(lists.begin(), p);
+  std::vector<std::pair<int, std::forward_list<int>*>> heap;
+  
+  for (int i = 0; i < k; ++i) {
+    auto l =  va_arg(args, std::forward_list<int>*);
+    if (!l->empty()) {
+      heap.push_back(std::make_pair(l->front(), l));
+      l->pop_front();
     }
-    return lists.size()==1 ? lists[0] : NULL;
+  }
 
-    /* the following method is not fast enough! */
-    /*
-    ListNode* pHead = NULL;
-    for (int i=0; i<lists.size(); i++){
-        pHead = mergeTwoLists(pHead, lists[i]);
+  std::make_heap(heap.begin(), heap.end());
+
+  auto result = new std::forward_list<int>();
+
+  while (heap.size() > 0) {
+    std::pop_heap(heap.begin(), heap.end());
+    result->push_front(heap.back().first);
+    auto l = heap.back().second;
+    if (!l->empty()) {
+      heap.back().first = l->front();
+      l->pop_front();
+      std::push_heap(heap.begin(), heap.end());
+    } else {
+      heap.pop_back();
     }
-    return pHead;
-    */
+  }
+
+  result->reverse();
+  return result;
 }
 
-static int n=0;
-ListNode *mergeTwoLists(ListNode* head1, ListNode* head2){
-    if (n){
-        //cout << "------ method 01 ------" <<endl;
-        return mergeTwoLists01(head1, head2);
-    }
-    //cout << "------ method 02 ------" <<endl;
-    return mergeTwoLists02(head1, head2);
-}
-/*======================================================================*/
-/*                            Method One                                */
-/*======================================================================*/
+int main() {
+  std::forward_list<int> v1 {4 ,3, 1};
+  std::forward_list<int> v2 {};
+  std::forward_list<int> v3 {2};
 
-//#define INT_MAX   2147483647
-//#define INT_MIN   (-INT_MAX - 1)
+  std::forward_list<int>* r = MergeKSortedLists(3, &v1, &v2, &v3);
+  std::copy(r->begin(), r->end(), std::ostream_iterator<int>(std::cout, " "));
+  delete r;
 
-ListNode *mergeTwoLists01(ListNode* head1, ListNode* head2){
-    ListNode *p1 = head1,  *p2=head2;
-    static ListNode dummy(0);
-
-    dummy.next = p1;
-    ListNode *prev = &dummy;
-
-    while(p1 && p2){
-        if(p1->val < p2->val){
-            prev = p1;
-            p1 = p1->next;
-        }else{
-            prev->next = p2;
-            p2 = p2->next;
-            prev = prev->next;
-            prev->next = p1;
-        }
-    }
-    if (p2){
-        prev->next = p2;
-    }
-
-    return dummy.next;
-}
-
-/*======================================================================*/
-/*                            Method Two                                */
-/*======================================================================*/
-
-
-void TakeOutNode(ListNode*& head, ListNode*& tail, ListNode*& p);
-
-ListNode *mergeTwoLists02(ListNode* head1, ListNode* head2) {
-    ListNode *p1 = head1,  *p2=head2;
-
-    ListNode *pHead = NULL, *pTail=NULL;
-    while(p1 && p2){
-        if(p1->val < p2->val){
-            TakeOutNode(pHead, pTail, p1);
-        }else{
-            TakeOutNode(pHead, pTail, p2);
-        }
-    }
-
-    ListNode *p=NULL;
-    if (p1){
-        p = p1;
-    }else if (p2){
-        p = p2;
-    }
-    if (pHead==NULL){
-        return p;
-    }
-    pTail->next = p;
-    return pHead;
-}
-
-void TakeOutNode(ListNode*& head, ListNode*& tail, ListNode*& p){
-    ListNode *pNext = p->next;
-    if (head==NULL){
-        head = tail = p;
-    }else{
-        tail->next = p;
-        tail = p;
-    }
-    p->next = NULL;
-    p = pNext;
-}
-
-void printList(ListNode* h)
-{
-    while(h!=NULL){
-        printf("%d ", h->val);
-        h = h->next;
-    }
-    printf("\n");
-}
-
-ListNode* createList(int a[], int n)
-{
-    ListNode *head=NULL, *p=NULL;
-    for(int i=0; i<n; i++){
-        if (head == NULL){
-            head = p = new ListNode(a[i]);
-        }else{
-            p->next = new ListNode(a[i]);
-            p = p->next;
-        }
-    }
-    return head;
-}
-
-
-int main(int argc, char**argv)
-{
-    srand(time(0));
-    if (argc>1){
-        n = atoi(argv[1]) % 2;
-    }
-
-    int a[] = {1,3,5,6,7,10};
-    int b[] = {0,4,6,8,9,11,20,30,40};
-    ListNode* p1 = createList(a, sizeof(a)/sizeof(int));
-    ListNode* p2 = createList(b, sizeof(b)/sizeof(int));
-
-    printList( mergeTwoLists(p1,p2) );
-    //mergeTwoLists(p1,p2) ;
-
-
-    vector<ListNode*> v;
-    for(int i=0; i<10240; i++) {
-        v.push_back(new ListNode(random()%100));
-    }
-    printList( mergeKLists(v) );
-    //mergeKLists(v);
-
-
-    cout << "method " << n+1 << endl;
-    return 0;
+  return 0;
 }
