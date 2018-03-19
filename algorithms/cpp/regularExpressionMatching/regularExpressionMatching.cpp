7b8c5735d9bdc9fb006c4458112d0183a4caaa63
Yi Wang <yi.wang.2005@gmail.com>
2018-03-19 13:58:25 -0700

    Remove recursive calls from isMatch

--- algorithms/cpp/regularExpressionMatching/regularExpressionMatching.cpp
+++ algorithms/cpp/regularExpressionMatching/regularExpressionMatching.cpp
@@ -26,46 +26,40 @@
 *               
 **********************************************************************************/
 
-#include <stdio.h>
-#include <string.h>
 #include <iostream>
 
-using namespace std;
-
-
-bool isMatch(const char *s, const char *p) {
-
-    if (*p=='\0') {
-        return *s == '\0';
-    }
-    //p's length 1 is special case 
-    if (*(p+1) == '\0' || *(p+1) !='*' ) {
-        if (*s=='\0' || ( *p !='.' && *s != *p )) {
-            return false;
-        }
-        return isMatch(s+1, p+1);
-    }
-    int len = strlen(s);
-    int i = -1;
-    while (i < len && (i <0 || *p=='.' || *p==*(s+i)) ){
-        if (isMatch(s+i+1, p+2)) {
-            return true;
-        }
-        i++;
-    }
+bool MatchRegexp(const char* s, const char* p) {
+  if (s == nullptr || p == nullptr) {
     return false;
+  }
+
+  while (*p != '\0') {
+    if (*(p+1) == '*') {
+      while ((*p != '.' && *s == *p) ||
+	     (*p == '.' && *s != '\0')) {
+	++s;
+      }
+      p += 2;
+    } else {
+      if ((*p != '.' && *s != *p) ||
+	  (*s == '\0')) {
+	return false;
+      }
+      ++p;
+      ++s;
+    } 
+  }
+
+  return *s == '\0';
 }
 
-
-int main(int argc, char** argv)
-{
-    const char* s = "aaa";
-    const char* p = "a.*";
-
-    if (argc>2) {
-        s = argv[1];
-        p = argv[2];
-    }
-
-    cout << s << ", " << p << " : " << isMatch(s,p) << endl;
+int main() {
+  std::cout << MatchRegexp("aa","a")<< "\n";
+  std::cout << MatchRegexp("aa","aa")<< "\n";
+  std::cout << MatchRegexp("aaa","aa")<< "\n";
+  std::cout << MatchRegexp("aa", "a*")<< "\n";
+  std::cout << MatchRegexp("aa", ".*")<< "\n";
+  std::cout << MatchRegexp("ab", ".*")<< "\n";
+  std::cout << MatchRegexp("aab", "c*a*b")<< "\n";
+  return 0;
 }
