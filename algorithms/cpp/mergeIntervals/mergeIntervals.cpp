584f1197356237413a4b667ca6e771f25e37b6d1
Yi Wang <yi.wang.2005@gmail.com>
2018-03-20 23:52:01 -0700

    Significantly shorten the code
    using std::pair and initializer list.

--- algorithms/cpp/mergeIntervals/mergeIntervals.cpp
+++ algorithms/cpp/mergeIntervals/mergeIntervals.cpp
@@ -13,63 +13,27 @@
 *               
 **********************************************************************************/
 
-#include <iostream>
-#include <vector>
 #include <algorithm>
-using namespace std;
+#include <iostream>
+#include <utility>
+#include <vector>
 
-struct Interval {
-    int start;
-    int end;
-    Interval() : start(0), end(0) {}
-    Interval(int s, int e) : start(s), end(e) {}
-};
+void MergeIntervals(std::vector<std::pair<int, int>>& intervals) {
+  std::sort(intervals.begin(), intervals.end());
 
-//Two factos sorting [start:end]
-bool compare(const Interval& lhs, const Interval& rhs){
-    return (lhs.start==rhs.start) ? lhs.end < rhs.end : lhs.start < rhs.start;
+  for (int i = 0; i < intervals.size() - 1; ++i) {
+    if (intervals[i+i].first < intervals[i].second) {
+      intervals[i].second = std::max(intervals[i].second, intervals[i+1].second);
+      intervals.erase(intervals.begin() + i + 1); 
+    }
+  }
 }
 
-vector<Interval> merge(vector<Interval> &intervals) {
-
-    vector<Interval> result;
-
-    if (intervals.size() <= 0) return result;
-    //sort the inervals. Note: using the customized comparing function.
-    sort(intervals.begin(), intervals.end(), compare);
-    for(int i=0; i<intervals.size(); i++) { 
-        int size = result.size();
-        // if the current intervals[i] is overlapped with previous interval.
-        // merge them together
-        if( size>0 && result[size-1].end >= intervals[i].start) {
-            result[size-1].end = max(result[size-1].end, intervals[i].end); 
-        }else{
-            result.push_back(intervals[i]);
-        }
-    }
-    
-    return result;
-}
-
-
-int main(int argc, char**argv)
-{
-    Interval i1(1,4);
-    Interval i2(0,2);
-    Interval i3(3,5);
-    Interval i4(15,18);
-    vector<Interval> intervals;
-    intervals.push_back(i1);
-    intervals.push_back(i2);
-    intervals.push_back(i3);
-    intervals.push_back(i4);
-    
-    vector<Interval> r = merge(intervals);
-    
-    for(int i=0; i<r.size(); i++){
-        cout << "[ " << r[i].start << ", " << r[i].end << " ] ";
-    }
-    cout <<endl;
-
-    return 0;
+int main() {
+  std::vector<std::pair<int, int>> intervals = {{1,3}, {2,6}, {8,10}, {15,18}};
+  MergeIntervals(intervals);
+  for (auto& v : intervals) {
+    std::cout << "(" << v.first << "," << v.second << "), ";
+  }
+  return 0;
 }
