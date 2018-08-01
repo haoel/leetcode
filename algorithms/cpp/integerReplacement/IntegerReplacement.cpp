7df02eed957468b5c6f1da123a20dce2e56ba5df
ehds <grephale@gmail.com>
2018-08-01 17:39:47 +0800

    New Solution "Integer Replacement"

--- algorithms/cpp/integerReplacement/IntegerReplacement.cpp
+++ algorithms/cpp/integerReplacement/IntegerReplacement.cpp
@@ -63,7 +63,22 @@ public:
         return result;
     }
     
+    int integerReplacement_simple(int n){
+        int ans = 0;
+        size_t m = n;
+        while (1 != m) {
+            if (1 == (m & 1)) {
+                if (m==3) --m; //special case
+                else m = (m&0b11^0b01) ? m + 1 : m - 1;
+            }
+            else m >>= 1;
+            ++ans;
+        }
+    return ans;
+    }
+
     int integerReplacement(int n) {
         return integerReplacement_recursionWithCache(n);
+        return integerReplacement_simple(n);
     }
 };
