4b2f4d01be706b379c1820bf4c77c8ce84568662
Hao Chen <haoel@hotmail.com>
2019-02-04 12:16:03 +0800

    optimzating the implementation of Unique Paths I/II

--- algorithms/cpp/uniquePaths/uniquePaths.II.cpp
+++ algorithms/cpp/uniquePaths/uniquePaths.II.cpp
@@ -53,4 +53,29 @@ int uniquePathsWithObstacles(vector<vector<int> > &obstacleGrid) {
     return max;
 }
 
+// the previous implemetation has too many if-else
+// the following dynamic programming is much more easy to read
+int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+    int row = obstacleGrid.size();
+    int col = obstacleGrid[0].size();
+
+    vector< vector <int> >  dp (row, vector<int>(col, 0));
+
+    dp[0][0] =  obstacleGrid[0][0]  ?  0 : 1;
+    for (int r=1; r<row; r++) {
+        dp[r][0] = obstacleGrid[r][0]  ?  0 : dp[r-1][0];
+    }
+    for (int c=1; c<col; c++) {
+        dp[0][c] = obstacleGrid[0][c]  ?  0 : dp[0][c-1];
+    }
+
+    for (int r=1; r<row; r++) {
+        for (int c=1; c<col; c++) {
+            dp[r][c]  = obstacleGrid[r][c] == 1 ? 0 : dp[r][c-1] + dp[r-1][c];
+        }
+    }
+
+    return dp[row-1][col-1];
+}
+
 
