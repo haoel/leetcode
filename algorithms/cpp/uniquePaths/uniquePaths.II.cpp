5f94d171bf15718a760736c1901cc6303812b713
ehds <ds_hale@163.com>
2019-02-26 20:47:53 +0800

    fix uniquePaths.II cpp solution

--- algorithms/cpp/uniquePaths/uniquePaths.II.cpp
+++ algorithms/cpp/uniquePaths/uniquePaths.II.cpp
@@ -33,8 +33,8 @@ using namespace std;
 int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
     vector<vector<unsigned int>>  v (row, vector<unsigned int>(col, 0));
     unsigned int max=0;
-    for (int i=0; i<obstacleGrid.size();; i++){
-        for (int j=0; j<obstacleGrid[0].size();; j++){
+    for (int i=0; i<obstacleGrid.size(); i++){
+        for (int j=0; j<obstacleGrid[0].size(); j++){
             if(obstacleGrid[i][j] == 1){
                 max = v[i][j] = 0;
             } else {
