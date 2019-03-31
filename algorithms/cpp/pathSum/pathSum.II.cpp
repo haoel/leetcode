07f864b236e3799df4ffa42a1f4f68cc6253836a
Hao Chen <haoel@hotmail.com>
2019-04-01 00:30:05 +0800

    simplified the implmentation

--- algorithms/cpp/pathSum/pathSum.II.cpp
+++ algorithms/cpp/pathSum/pathSum.II.cpp
@@ -41,32 +41,22 @@ public:
     vector<vector<int> > pathSum(TreeNode *root, int sum) {
         vector<vector<int> > result;
         vector<int> v;
-        generatePathSum(root, sum, v, result);
+        generatePathSum(root, sum, 0, v, result);
         return result;
     }
-    
-    void generatePathSum(TreeNode *root, int sum, vector<int> v, vector<vector<int> >& result) {
-        
-        if (root==NULL) {
-            return;
-        }
-        
-        if (root->left==NULL && root->right==NULL) {
-            if (root->val == sum){
-                v.push_back(root->val);
-                result.push_back(v);
-            }
-            return;
-        }
-        
+
+    void generatePathSum(TreeNode *root, int sum, int s, vector<int> v, vector<vector<int> >& result) {
+        if (root==NULL) return ;
+
+        s += root->val;
         v.push_back(root->val);
-        
-        if (root->left) {
-            generatePathSum(root->left, sum - root->val, v, result);
+
+        if ( root->left==NULL && root->right==NULL) {
+            if (s == sum) result.push_back(v);
+            return;
         }
-        
-        if (root->right) {
-            generatePathSum(root->right, sum - root->val, v, result);
-        }
+
+        generatePathSum(root->left, sum, s, v, result);
+        generatePathSum(root->right, sum, s, v, result);
     }
 };
