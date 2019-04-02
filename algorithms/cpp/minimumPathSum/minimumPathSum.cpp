4b062efa6c0b92529b9f8a594cbd88b95436f3ba
Hao Chen <haoel@hotmail.com>
2019-04-03 00:27:18 +0800

    update the comments

--- algorithms/cpp/minimumPathSum/minimumPathSum.cpp
+++ algorithms/cpp/minimumPathSum/minimumPathSum.cpp
@@ -2,14 +2,25 @@
 // Author : Hao Chen
 // Date   : 2014-06-21
 
-/********************************************************************************** 
-* 
-* Given a m x n grid filled with non-negative numbers, find a path from top left to 
-* bottom right which minimizes the sum of all numbers along its path.
-* 
-* Note: You can only move either down or right at any point in time.
-*               
-**********************************************************************************/
+/***************************************************************************************************** 
+ *
+ * Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right 
+ * which minimizes the sum of all numbers along its path.
+ * 
+ * Note: You can only move either down or right at any point in time.
+ * 
+ * Example:
+ * 
+ * Input:
+ * [
+ *   [1,3,1],
+ *   [1,5,1],
+ *   [4,2,1]
+ * ]
+ * Output: 7
+ * Explanation: Because the path 1&rarr;3&rarr;1&rarr;1&rarr;1 minimizes the sum.
+ * 
+ ******************************************************************************************************/
 
 #include <limits.h>
 #include <iostream>
