c4e92495bc3f4afb057e178d4da2b2a8b3f9f7bb
Hao Chen <haoel@hotmail.com>
2019-04-03 21:47:19 +0800

    update the problem description

--- algorithms/cpp/bestTimeToBuyAndSellStock/bestTimeToBuyAndSellStock.IV.cpp
+++ algorithms/cpp/bestTimeToBuyAndSellStock/bestTimeToBuyAndSellStock.IV.cpp
@@ -2,18 +2,29 @@
 // Author : Hao Chen
 // Date   : 2015-03-31
 
-/********************************************************************************** 
-* 
-* Say you have an array for which the ith element is the price of a given stock on day i.
-* 
-* Design an algorithm to find the maximum profit. You may complete at most k transactions.
-* 
-* Note:
-* You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
-* 
-* Credits:Special thanks to @Freezen for adding this problem and creating all test cases.
-*               
-**********************************************************************************/
+/***************************************************************************************************** 
+ *
+ * Say you have an array for which the ith element is the price of a given stock on day i.
+ * 
+ * Design an algorithm to find the maximum profit. You may complete at most k transactions.
+ * 
+ * Note:
+ * You may not engage in multiple transactions at the same time (ie, you must sell the stock before 
+ * you buy again).
+ * 
+ * Example 1:
+ * 
+ * Input: [2,4,1], k = 2
+ * Output: 2
+ * Explanation: Buy on day 1 (price = 2) and sell on day 2 (price = 4), profit = 4-2 = 2.
+ * 
+ * Example 2:
+ * 
+ * Input: [3,2,6,5,0,3], k = 2
+ * Output: 7
+ * Explanation: Buy on day 2 (price = 2) and sell on day 3 (price = 6), profit = 6-2 = 4.
+ *              Then buy on day 5 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+ ******************************************************************************************************/
 
 class Solution {
 public:
--- algorithms/cpp/bestTimeToBuyAndSellStock/bestTimeToBuyAndSellStock.cpp
+++ algorithms/cpp/bestTimeToBuyAndSellStock/bestTimeToBuyAndSellStock.cpp
@@ -2,14 +2,29 @@
 // Author : Hao Chen
 // Date   : 2014-06-18
 
-/********************************************************************************** 
-* 
-* Say you have an array for which the ith element is the price of a given stock on day i.
-* 
-* If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), 
-* design an algorithm to find the maximum profit.
-*               
-**********************************************************************************/
+/***************************************************************************************************** 
+ *
+ * Say you have an array for which the ith element is the price of a given stock on day i.
+ * 
+ * If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of 
+ * the stock), design an algorithm to find the maximum profit.
+ * 
+ * Note that you cannot sell a stock before you buy one.
+ * 
+ * Example 1:
+ * 
+ * Input: [7,1,5,3,6,4]
+ * Output: 5
+ * Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+ *              Not 7-1 = 6, as selling price needs to be larger than buying price.
+ * 
+ * Example 2:
+ * 
+ * Input: [7,6,4,3,1]
+ * Output: 0
+ * Explanation: In this case, no transaction is done, i.e. max profit = 0.
+ * 
+ ******************************************************************************************************/
 
 class Solution {
 public:
