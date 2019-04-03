b98faeec1e2adbc25ebacbc1b533603759bb033b
Hao Chen <haoel@hotmail.com>
2019-04-03 21:47:19 +0800

    update the problem description

--- algorithms/cpp/bestTimeToBuyAndSellStock/bestTimeToBuyAndSellStock.II.cpp
+++ algorithms/cpp/bestTimeToBuyAndSellStock/bestTimeToBuyAndSellStock.II.cpp
@@ -2,16 +2,37 @@
 // Author : Hao Chen
 // Date   : 2014-06-18
 
-/********************************************************************************** 
-* 
-* Say you have an array for which the ith element is the price of a given stock on day i.
-* 
-* Design an algorithm to find the maximum profit. You may complete as many transactions 
-* as you like (ie, buy one and sell one share of the stock multiple times). However, 
-* you may not engage in multiple transactions at the same time (ie, you must sell the 
-* stock before you buy again).
-*               
-**********************************************************************************/
+/***************************************************************************************************** 
+ *
+ * Say you have an array for which the ith element is the price of a given stock on day i.
+ * 
+ * Design an algorithm to find the maximum profit. You may complete as many transactions as you like 
+ * (i.e., buy one and sell one share of the stock multiple times).
+ * 
+ * Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock 
+ * before you buy again).
+ * 
+ * Example 1:
+ * 
+ * Input: [7,1,5,3,6,4]
+ * Output: 7
+ * Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+ *              Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+ * 
+ * Example 2:
+ * 
+ * Input: [1,2,3,4,5]
+ * Output: 4
+ * Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+ *              Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are
+ *              engaging multiple transactions at the same time. You must sell before buying again.
+ * 
+ * Example 3:
+ * 
+ * Input: [7,6,4,3,1]
+ * Output: 0
+ * Explanation: In this case, no transaction is done, i.e. max profit = 0.
+ ******************************************************************************************************/
 
 class Solution {
 public:
