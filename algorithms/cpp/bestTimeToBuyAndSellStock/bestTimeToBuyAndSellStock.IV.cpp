14efb7e2999f9e295f0638efc074095a2f429c38
Hao Chen <haoel@hotmail.com>
2019-04-04 01:43:59 +0800

    refactory the code : show the evolution of the solution

--- algorithms/cpp/bestTimeToBuyAndSellStock/bestTimeToBuyAndSellStock.IV.cpp
+++ algorithms/cpp/bestTimeToBuyAndSellStock/bestTimeToBuyAndSellStock.IV.cpp
@@ -27,61 +27,109 @@
  ******************************************************************************************************/
 
 class Solution {
-public:
-    /*
-     *  dp[i, j]
-     *    - `i` represents the number of transactions we've done so far. ( 0 <= i <= k )
-     *    - `j` represents the number of days so far. ( 0 <= j <= prices.size() )
-     *  
-     *  So, we have the initialization as below:
-     *
-     *  dp[0, j] = 0; // 0 <= j <= prices.size() 
-     *  dp[i, 0] = 0; // 0 <= i <= k
-     *  
-     *  And the iteration logic as below:
-     *  
-     *  dp[i, j] = max ( 
-     *               dp[i, j-1], // same times transactions, but one days before.    
-     *               dp[i-1, t] + prices[j] - prices[t+1]  // for all of (0 <= t < j )
-     *                                                     // this means find max profit from previous any of days
-     *           )
-     *  
-     */
+    public:
+        /*
+           profits[trans, day]
+           - `trans` represents the number of transactions we've done so far. ( 0 <= trans <= k )
+           - `day` represents the number of days so far. ( 0 <= day <= prices.size() )
 
-    int maxProfit(int k, vector<int> &prices) {
-        int ndays = prices.size();
-        
-        // error case
-        if (ndays<=1) return 0;
-        
-        // Edge case
-        // ---------
-        // if the number of transactions is too many, it means we can make 
-        // as many transactions as we can, that brings us the problem back to 
-        // Best-Time-To-Buy-And-Sell-Stock-II which can be simply solve in O(n) 
-        // by using a greedy approach.
-        if(k > ndays/2){
-            int sum = 0;
-            for (int i=1; i<ndays; i++) {
-                sum += max(prices[i] - prices[i-1], 0);
+           So, we have the initialization as below:
+
+           profits[0, day] = 0; // 0 <= day <= prices.size()
+           profits[trans, 0] = 0; // 0 <= trans <= k
+
+           And the iteration logic as below:
+
+           profits[trans, day] = max (
+           profits[trans, day-1], // same times transactions, but one days before.
+           profits[trans-1, i-1] + (prices[day] - prices[i])// for all of (0 <= i < day )
+        // this means find max profit from
+        // previous any of days
+        )
+
+*/
+
+        int maxProfit(int k, vector<int> &prices) {
+            int ndays = prices.size();
+
+            // error case
+            if (ndays<=1) return 0;
+
+            // Edge case
+            // ---------
+            // if the number of transactions is too many, it means we can make
+            // as many transactions as we can, that brings us the problem back to
+            // Best-Time-To-Buy-And-Sell-Stock-II which can be simply solve in O(n)
+            // by using a greedy approach.
+            if(k > ndays/2){
+                int sum = 0;
+                for (int i=1; i<ndays; i++) {
+                    sum += max(prices[i] - prices[i-1], 0);
+                }
+                return sum;
             }
-            return sum;
+
+            return maxProfit_DP03(k, prices);//8ms
+            return maxProfit_DP02(k, prices);//8ms
+            return maxProfit_DP01(k, prices);//492ms
         }
-        
-        //DP solution - O(kn) complexity 
-        vector< vector<int> > dp (k+1, vector<int>( ndays+1, 0));
-        
-        for(int i=1; i<=k; i++) {
-            int t = dp[i-1][0] - prices[0];
-            for (int j=1; j <= ndays; j++) {
-                dp[i][j] = max(dp[i][j-1], t + prices[j-1]);
-                if ( j < ndays ) {
-                    t = max(t, dp[i-1][j] - prices[j]);
+
+
+        //DP solution - O(kn^2) complexity
+        int maxProfit_DP01 (int k, vector<int> &prices) {
+            int ndays = prices.size();
+            vector< vector<int> > profits(k+1, vector<int>( ndays+1, 0));
+            for(int trans=1; trans<=k; trans++) {
+                for (int day=1; day<=ndays; day++) {
+                    int m=0;
+                    for (int i=1; i<=day; i++) {
+                        m = max(m, profits[trans-1][i-1]+ prices[day-1] - prices[i-1]);
+                    }
+                    profits[trans][day] =  max( profits[trans][day-1], m);
                 }
             }
+            return profits[k][ndays];
         }
-        
-        return dp[k][ndays];
-        
-    }
+
+        //DP solution - O(kn) complexity
+        //Actually, we could save the loop in above- for(int i=1; i<=day; i++)
+        //Becasue there are so many dupliations
+
+        int maxProfit_DP02 (int k, vector<int> &prices) {
+
+            int ndays = prices.size();
+
+            vector< vector<int> > profits(k+1, vector<int>( ndays+1, 0));
+            vector<int> m(ndays+1, 0); // tracking the max profits of previous days
+
+            for(int trans=1; trans<=k; trans++) {
+                m[0] = profits[trans-1][0] - prices[0];
+                for (int day=1; day<=ndays; day++) {
+                    profits[trans][day] =  max( profits[trans][day-1], m[day-1]+prices[day-1]);
+                    if (day < ndays) {
+                        m[day] = max(m[day-1], profits[trans-1][day] - prices[day]);
+                    }
+                }
+            }
+            return profits[k][ndays];
+        }
+
+
+        // save the memory, remove the m[ ] array
+        int maxProfit_DP03 (int k, vector<int> &prices) {
+            int ndays = prices.size();
+            vector< vector<int> > profits(k+1, vector<int>( ndays+1, 0));
+
+            for(int trans=1; trans<=k; trans++) {
+                int m = profits[trans-1][0] - prices[0];
+                for (int day=1; day <= ndays; day++) {
+                    profits[trans][day] = max(profits[trans][day-1], m + prices[day-1]);
+                    if ( day < ndays ) {
+                        m = max(m, profits[trans-1][day] - prices[day]);
+                    }
+                }
+            }
+
+            return profits[k][ndays];
+        }
 };
