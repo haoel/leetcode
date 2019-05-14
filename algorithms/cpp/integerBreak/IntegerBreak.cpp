8a1623ecf335403dc9d9216f5148189542578ea4
TaoWei <36024756+LynnTh@users.noreply.github.com>
2019-05-14 09:36:20 +0800

    Update IntegerBreak.cpp

--- algorithms/cpp/integerBreak/IntegerBreak.cpp
+++ algorithms/cpp/integerBreak/IntegerBreak.cpp
@@ -44,3 +44,16 @@ public:
     }
 };
 
+// DP
+class Solution {
+public:
+    int integerBreak(int n) {
+        vector<int> dp(n+1,1);
+        for(int i=2;i<=n;i++){
+            for(int j=1;j<=i/2;j++){
+                dp[i] = max(dp[i],max(dp[j],j)*max(dp[i-j],i-j));
+            }
+        }
+        return dp[n];
+    }
+};
