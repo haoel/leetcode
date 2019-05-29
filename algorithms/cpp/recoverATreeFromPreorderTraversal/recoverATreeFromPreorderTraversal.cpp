1ddea2815cdf70eece3f126259b33cf0b1ef47b4
Hao Chen <haoel@hotmail.com>
2019-05-29 17:22:42 +0800

    refine the file name and the problem description comment #172

--- algorithms/cpp/recoverATreeFromPreorderTraversal/recoverATreeFromPreorderTraversal.cpp
+++ algorithms/cpp/recoverATreeFromPreorderTraversal/recoverATreeFromPreorderTraversal.cpp
@@ -1,29 +1,40 @@
+// Source : https://leetcode.com/problems/recover-a-tree-from-preorder-traversal/
+// Author : Ahmed Morsy
+// Date   : 2019-05-29
 // source : https://leetcode.com/problems/recover-a-tree-from-preorder-traversal/
-// Author Ahmed Morsy
-// Date   : 2019-5-19
-
-/********************************************************************************** 
-*We run a preorder depth first search on the root of a binary tree.
-*At each node in this traversal, we output D dashes (where D is the depth of this node), 
-*then we output the value of this node.  
-*(If the depth of a node is D, the depth of its immediate child is D+1.The depth of the root node is 0.)
-*If a node has only one child, that child is guaranteed to be the left child.
-*Given the output S of this traversal, recover the tree and return its root.
-
-* Input: "1-2--3--4-5--6--7"
-* Output: [1,2,5,3,4,6,7]
-
-* Proposed Solution
-  -----------------
- 1. for each node in the input save its depth and value
- 2. start a stack with the root node pushed onto it
-    
-    IF the following node has the root's depth + 1 and the root has less than 2 children then this node
-    is a child for this root. Add it to the stack.
-    
-    ELSE then pop the current node from the stack.
-    
-**********************************************************************************/
+/***************************************************************************************************** 
+ *
+ * We run a preorder depth first search on the root of a binary tree.
+ * 
+ * At each node in this traversal, we output D dashes (where D is the depth of this node), then we 
+ * output the value of this node.  (If the depth of a node is D, the depth of its immediate child is 
+ * D+1.  The depth of the root node is 0.)
+ * 
+ * If a node has only one child, that child is guaranteed to be the left child.
+ * 
+ * Given the output S of this traversal, recover the tree and return its root.
+ * 
+ * Example 1:
+ * 
+ * Input: "1-2--3--4-5--6--7"
+ * Output: [1,2,5,3,4,6,7]
+ * 
+ * Example 2:
+ * 
+ * Input: "1-2--3---4-5--6---7"
+ * Output: [1,2,5,3,null,6,null,4,null,7]
+ * 
+ * Example 3:
+ * 
+ * Input: "1-401--349---90--88"
+ * Output: [1,401,null,349,88,90]
+ * 
+ * Note:
+ * 
+ * 	The number of nodes in the original tree is between 1 and 1000.
+ * 	Each node will have a value between 1 and 10^9.
+ * 
+ ******************************************************************************************************/
 
 /*  Definition for a binary tree node.
   struct TreeNode {
