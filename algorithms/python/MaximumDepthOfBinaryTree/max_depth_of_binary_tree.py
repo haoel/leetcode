ad182e139bceb66b51fbbb39ce6b61a23a670aa9
Arnab Chakraborty <arnab033chakraborty@gmail.com>
2020-07-19 21:45:54 +0530

    New Problem Solution - "559: Maximum Depth Of Binary Tree"

---
+++ algorithms/python/MaximumDepthOfBinaryTree/max_depth_of_binary_tree.py
@@ -0,0 +1,36 @@
+# Source : https://leetcode.com/problems/maximum-depth-of-n-ary-tree/
+# Author : Arnab Chakraborty
+# Date   : 2019-07-19
+
+#**********************************************************************************
+#
+# Given a binary tree, find its maximum depth.
+#
+# The maximum depth is the number of nodes along the longest path from the root node
+# down to the farthest leaf node.
+#
+#**********************************************************************************
+
+
+
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def maxDepth(self, root: 'Node', depth=1) -> int:
+        if root is None:
+            return 0
+        
+        depth = 0
+        for child in root.children:
+            depth = max([depth, self.maxDepth(child)])
+            
+        print(root.val, depth + 1)
+            
+        return depth + 1
