65292243f3df62cf47910b0f6014a11cab5fa3e3
Hao Chen <haoel@hotmail.com>
2020-10-02 16:32:12 +0800

    adjusted the comment and add the solution " Time Needed to Inform All Employees\ into README.md - fix #215"

--- algorithms/cpp/timeNeededToInformAllEmployees/TimeNeededToInformAllEmployees.cpp
+++ algorithms/cpp/timeNeededToInformAllEmployees/TimeNeededToInformAllEmployees.cpp
@@ -4,14 +4,20 @@
 
 /*************************************************************************************** 
  *
- * A company has n employees with a unique ID for each employee from 0 to n - 1. The head of the company has is the one with headID.
-
- * Each employee has one direct manager given in the manager array where manager[i] is the direct manager of the i-th employee, manager[headID] = -1. Also it's guaranteed that the subordination relationships have a tree structure.
-
- * The head of the company wants to inform all the employees of the company of an urgent piece of news. He will inform his direct subordinates and they will inform their subordinates and so on until all employees know about the urgent news.
-
- * The i-th employee needs informTime[i] minutes to inform all of his direct subordinates (i.e After informTime[i] minutes, all his direct subordinates can start spreading the news).
-
+ * A company has n employees with a unique ID for each employee from 0 to n - 1. 
+ * The head of the company has is the one with headID.
+ *
+ * Each employee has one direct manager given in the manager array where manager[i] is 
+ * the direct manager of the i-th employee, manager[headID] = -1. Also it's guaranteed 
+ * that the subordination relationships have a tree structure.
+ *
+ * The head of the company wants to inform all the employees of the company of an urgent 
+ * piece of news. He will inform his direct subordinates and they will inform their 
+ * subordinates and so on until all employees know about the urgent news.
+ *
+ * The i-th employee needs informTime[i] minutes to inform all of his direct subordinates 
+ * (i.e After informTime[i] minutes, all his direct subordinates can start spreading the news).
+ *
  * Return the number of minutes needed to inform all the employees about the urgent news.
  * 
  * Example:
