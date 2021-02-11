d50de4612d6a2e500239bbe9edeb0641b7ce5788
tian <tianwu@outlook.com>
2021-02-11 18:28:07 +1100

    fix typo (#246)

--- algorithms/python/ContainsDuplicateII/containsNearbyDuplicate.py
+++ algorithms/python/ContainsDuplicateII/containsNearbyDuplicate.py
@@ -15,7 +15,7 @@ def containsNearbyDuplicate(self, nums, k):
 def containsNearbyDuplicate(self, nums, k):
     d = {}
     for i, n in enumerate(nums):
-        if n in d and i - d[v] <= k:
+        if n in d and i - d[n] <= k:
             return True
         d[n] = i
     return False
