81a3fa89c1796236a05622e9cae5841d8ea7a940
Hao Chen <haoel@hotmail.com>
2021-04-21 20:35:36 +0800

    another alogrithm only one loop

--- algorithms/cpp/maximumXorForEachQuery/MaximumXorForEachQuery.cpp
+++ algorithms/cpp/maximumXorForEachQuery/MaximumXorForEachQuery.cpp
@@ -50,6 +50,21 @@
 class Solution {
 public:
     vector<int> getMaximumXor(vector<int>& nums, int maximumBit) {
+
+        const int size = nums.size();
+        const int max = (1 << maximumBit) - 1;
+
+        int all = 0;
+        vector<int> result(size);
+        for(int i = 0; i < size; i++) {
+            all ^= nums[i];
+            result[size-i-1] = all ^ max;
+        }
+
+        return result;
+    }
+
+    vector<int> getMaximumXor(vector<int>& nums, int maximumBit) {
         int all = 0;
         for(auto& n : nums) {
             all ^= n;
