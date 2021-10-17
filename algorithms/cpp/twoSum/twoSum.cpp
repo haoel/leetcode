53f17844ca83d026bd234b7d600abb32407efc23
Pier Carlo Cadoppi <pc.cadoppi@gmail.com>
2021-10-17 17:14:13 +0200

    grammar

--- algorithms/cpp/twoSum/twoSum.cpp
+++ algorithms/cpp/twoSum/twoSum.cpp
@@ -46,8 +46,8 @@ public:
     //
     //  1) Traverse the array one by one
     //  2) just put the `target - num[i]`(not `num[i]`) into the map
-    //     so, when we checking the next num[i], if we found it is exisited in the map.
-    //     which means we found the second one.
+    //     so, when we checking the next num[i], if we found it existed in the map,
+    //     it means we found the second one.
     //      
     vector<int> twoSum(vector<int> &numbers, int target) {
         unordered_map<int, int> m;
@@ -55,7 +55,7 @@ public:
         for(int i=0; i<numbers.size(); i++){
             // not found the second one
             if (m.find(numbers[i])==m.end() ) { 
-                // store the first one poisition into the second one's key
+                // store the first one position into the second one's key
                 m[target - numbers[i]] = i; 
             }else { 
                 // found the second one
