66fdb8de49976c0ce1195b76d1dec6ab94bca895
UtkarshM-Hub <utmandape4@gmail.com>
2022-07-17 17:59:27 +0530

    Fixed the Input Array

--- algorithms/cpp/shuffleString/ShuffleString.cpp
+++ algorithms/cpp/shuffleString/ShuffleString.cpp
@@ -13,7 +13,7 @@
  * 
  * Example 1:
  * 
- * Input: s = "codeleet", indices = [4,5,6,7,0,2,1,3]
+ * Input: s = "codeleet", indices = [4,5,6,7,0,1,2,3]
  * Output: "leetcode"
  * Explanation: As shown, "codeleet" becomes "leetcode" after shuffling.
  * 
